ba9adcb82117496b7f920f1101bc8e1b57b35f93
dkamins314 <42924499+dkamins314@users.noreply.github.com>
2018-10-15 18:02:13 -0500

    Build a blog Final
    corrected and functioning

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app.secret_key='56ka52dh10WQOPHT$%$%$'
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)   
     title = db.Column(db.String(120))
-    body = db.Column(db. String(1000))
+    body = db.Column(db.String(1000))
 
 
     def __init__(self, title, body):
@@ -33,27 +33,27 @@ def index():
 
 @app.route('/blog', methods=['POST','GET'])
 def display_blogs():
-   blog_id =request.args.get(id) 
+   blog_id =request.args.get('id') 
    if (blog_id):
       blog=Blog.query.get(blog_id)
-      return render_template('single_blog.html', title= 'Blog Title', blog='blog') 
+      return render_template('single_blog.html', title="Blog Title", blog= blog) 
    else:
         all_blogs = Blog.query.all() 
 
-   return render_template('all_blogs.html', title="All Blogs", all_blogs=all_blogs)
+        return render_template('all_blogs.html', title="All Blogs", all_blogs=all_blogs)
   
 @app.route('/new_blog', methods=['Post', 'GET'] )
 def new_blog_entry():
     if request.method == 'POST':
-        new_title = request.files ['title'] 
-        new_body = request.files ['body']
+        new_title = request.form ['title'] 
+        new_body = request.form ['body']
         new_post = Blog(new_title, new_body)
 
-        if new_blog_post.is_valid():
+        if new_post .is_valid():
             db.session.add(new_post)
             db.session.commit()
-           # url= "/blog?id="+str(new_post.id)
-           # return redirect('url')
+            url= "/blog?id="+str(new_post.id)
+            return redirect(url)
         else:
             flash("Please check your entry for errors or missing fields. Both title and body are required")
             return render_template('new_blog.html', title = "Create new blog entry", new_blog_title = new_blog_title,
